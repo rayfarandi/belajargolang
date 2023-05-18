26cc76d7a22d7bfe776761ea31c824798b72ac15
rayfarandi <rayfarandi1994@gmail.com>
2023-05-18 20:58:55 +0700

    menambah golang

---
+++ komentar.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hellow nama saya Ray") // ini koment
+	fmt.Println("Sedang Belajar GO")
+	/* ini koment
+	koment berbaris2
+	nihhh*/
+}
---
+++ tugas1.go
@@ -0,0 +1,7 @@
+package main
+
+import	"fmt"
+
+func main(){
+	fmt.Println("Nama Saya Ray Farandi");
+}
